3af3cd5058cd6759eb210ce5469d2ae3180c037f
Andrew-Ashraf22 <androa45@yahoo.com>
2023-09-23 05:53:43 -0700

    task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+/**
+ * bubble_sort - the bubble sort algo
+ * @array: the array
+ * @size: size of array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t j = 0, i = 0;
+	int temp, breaker;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		breaker = 0;
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				breaker = 1;
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+		if (breaker == 0)
+			return;
+	}
+}
